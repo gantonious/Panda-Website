da04324b53af703fae295f4ba673150847cf1c66
George Antonious <george@antonious.ca>
2020-07-11 14:29:29 -0400

    Updates

---
+++ precache-manifest.d47b23712944c5dcba6d8149beb06996.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40b8e41444208c96cea56670cf82b06",
+    "url": "/panda-website/index.html"
+  },
+  {
+    "revision": "16c63fcbf41cfea7a9c8",
+    "url": "/panda-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec182c6bec4ab4fa6d1b",
+    "url": "/panda-website/static/js/2.23ca9163.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/panda-website/static/js/2.23ca9163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c63fcbf41cfea7a9c8",
+    "url": "/panda-website/static/js/main.b5d635c0.chunk.js"
+  },
+  {
+    "revision": "a67c660d0d64187ecb16",
+    "url": "/panda-website/static/js/runtime-main.8dac893a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/panda-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
