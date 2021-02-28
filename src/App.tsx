86bd294dce9a27209750d494a2409d2b2cfdee5c
George Antonious <george@antonious.ca>
2021-02-27 22:02:02 -0500

    Add screenshots

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import logo from './panda_1024.png';
+import subreddit from './assets/subreddit.png'
+import panda1 from './assets/panda1.png'
+import panda2 from './assets/panda2.png'
+import panda3 from './assets/panda3.png'
+import panda4 from './assets/panda4.png'
+import panda5 from './assets/panda5.png'
 import './App.css';
+
+type SectionProps = {
+  title: string
+};
+
+function Section(props: SectionProps) {
+  return (
+    <div className="Section">
+      <div className="SectionContent">
+          <p className="App-title">
+            <strong>{props.title}</strong>
+          </p>
+          <p>Look at this nice subreddit</p>
+      </div>
+
+      <div className="ImageContent">
+        <img src={subreddit} width='300'/>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -12,7 +39,15 @@ function App() {
         </p>
         <p className="App-title-subtext">A new way to experience Reddit on iOS coming soon</p>
 
-        <p className="footer">Copyright 2020 © George Antonious</p>
+        <div className="app-images">
+          <img src={panda1} width='300'/>
+          <img src={panda3} width='300'/>
+          <img src={panda2} width='300'/>
+          <img src={panda4} width='300'/>
+          <img src={panda5} width='300'/>
+        </div>
+
+        {/* <p className="footer">Copyright 2020 © George Antonious</p> */}
       </header>
     </div>
   );
